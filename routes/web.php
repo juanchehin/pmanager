811616fccd9504716f08435e6cec0c4dfafb55ce
Juan Chehin SGM <chehinjuan@gmail.com>
2020-10-22 08:29:54 -0300

    Se creao la estructura de los modelos

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,10 @@ Route::get('/quien', function () {
 });
 
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
